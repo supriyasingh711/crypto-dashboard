8d61df2adb53723b4ced6fdbab67afec27efdd96
Supriya 007 <singhsupriya711@gmail.com>
2024-03-01 00:06:58 +0530

    todays final commit

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
+  base:"/crypto-dashboard/",
   build:{
     outDir:"./docs"
   },
-  base:"/crypto-dashboard/",
-  plugins: [react()],
 })
